d4a94ac507977fa7d0eb1a8f94f25e4b2fdc53af
chuunibyouruna <storedatangoc@gmail.com>
2019-01-09 16:51:59 +0700

    frame

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const pug = require('pug');
+const upload = require('express-fileupload');
+
+const router = require('./routes/router');
+
+const port = 3000;
+
+const app = express();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.use(router);
+app.listen(port,()=> console.log(`Server is started on port ${port}`));
