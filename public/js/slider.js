5540ca079e6356aa9d7832b498dd5b4291902ed4
unknown <jim.dangquoc706@gmail.com>
2019-02-09 22:32:21 -0800

    complete layout home for desktop

---
+++ public/js/slider.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+const slider = document.querySelector('.slider');
+
+function showSlides(n) {
+    const slides = slider.querySelectorAll('.slide');
+    const dots = document.querySelectorAll('.dot');
+
+    if (n > slides.length) slideIndex = 1;
+    if (n < 1) slideIndex = slides.length;
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+    dots.forEach(dot => {
+        dot.classList.remove('active');
+    })
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('active');
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+showSlides(slideIndex);
+
+setInterval(() => {
+    plusSlides(1);
+}, 7000);
